5ccdb72f82951cac8e03de435de6853884fbba2a
James Gasek <james@gasek.net>
2024-11-06 16:14:09 -0500

    progress

---
+++ Newton Notes/WorkoutView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutView.swift
+//  Newton Notes
+//
+//  Created by James Gasek on 11/5/24.
+//
+
+import SwiftUI
+
+struct WorkoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutView()
+}
